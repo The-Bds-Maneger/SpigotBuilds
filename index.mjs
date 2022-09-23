aa4397bfc750475788dc61ead21fdc61a96fe185
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-09-23 00:48:21 +0000

    Update

--- index.mjs
+++ index.mjs
@@ -8,5 +8,5 @@ const ver = process.argv[2]||"latest";
 child_process.execFileSync("java", ["-jar", await saveFile("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"), "-o", process.cwd(), "--rev", ver], {cwd: os.tmpdir(), stdio: "inherit"});
 if (ver !== "latest") {
   await fs.rm(path.join(process.cwd(), "Spigot.jar"), {force: true}).catch(() => null);
-  await fs.rename(path.join(process.cwd(), (await fs.readdir(path.join(process.cwd()))).find(file => file.endsWith(".jar"))), path.join(process.cwd(), "Spigot.jar"), {force: true});
+  await fs.rename(path.join(process.cwd(), (await fs.readdir(path.join(process.cwd()))).find(file => (file.startsWith("Spigot")||file.startsWith("spigot")) && file.endsWith(".jar"))), path.join(process.cwd(), "Spigot.jar"), {force: true});
 }
